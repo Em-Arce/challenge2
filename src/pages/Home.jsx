09dce1d8b3275696bf8a8271d0f5e1e7e4b43d4a
Em-Arce <emjenarce@hotmail.com>
2023-01-03 18:41:56 +0800

    Add increase, decrease, reset and fetch single doc from db

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,24 +1,55 @@
 import React from "react";
 import { useState, useEffect, useRef } from "react"; 
 import { firestore } from "../firebase";
-import { addDoc, collection } from "@firebase/firestore";
+import { addDoc, collection, doc, 
+  query, orderBy, onSnapshot
+} from "@firebase/firestore";
 
 export default function Home() {
-  const [inputValue, setInputValue] = useState(0);
-  const previousInputValue = useRef(0);
   const reference = collection(firestore, "counts");
 
-  useEffect(() => {
-    previousInputValue.current = inputValue;
-  }, [inputValue]);
+  const [currentValue, setCurrentValue] = useState("");
+
+  window.addEventListener( "load", () => {
+    fetchCurrentDatum();
+  });
+
+  const fetchCurrentDatum = doc(firestore, "counts", "UqUyiscWk2f8Dy8OrJYW");
+  // getDoc(fetchCurrentDatum)
+  //   .then((doc) => {
+  //     console.log(doc.data(), doc.id);
+  //     var data = doc.data().count;
+  //     setCurrentValue(data);
+  // });
+
+  onSnapshot(fetchCurrentDatum, (doc) => {
+      // console.log(doc.data(), doc.id);
+      var data = doc.data().currentCount;
+      setCurrentValue(data);
+  });
+
+  const [counter, setCounter] = useState(0);
+  // console.log(counter)
+  const increment = () => {
+    setCounter(count => count + 1);
+  };
+
+  const decrement = () => {
+    setCounter(count => count - 1);
+  };
+
+  const reset = () => {
+    setCounter(0);
+  };
 
   let data = {
-    count: inputValue,
+    currentCount: counter,
   }
 
   const handleSave = async (e) => {
     e.preventDefault();
-    console.log(inputValue);
+    console.log(counter);
+    alert("Counter saved")
     try {
       addDoc(reference, data);
     } catch(error) {
@@ -26,19 +57,27 @@ export default function Home() {
     }
   };
 
+  
+
+  //https://www.youtube.com/watch?v=7aDG3L-bTS8
+  // TODO: getalldata, then try orderby and limit
+
   return (
     <div>
+      
+      <h2>Current Value: {currentValue}</h2>
+      <h2>Counter Value: {counter}</h2>
+      <button onClick={increment}>+</button> <br></br>
+      <button onClick={decrement}>-</button> <br></br>
+      <button onClick={reset}>Reset</button>
       <form onSubmit={handleSave}>
-        <label>Enter Count:</label>
+        <label>Counter:</label>
         <input 
           type="number" 
-          value={inputValue}
-          onChange={ (e) => setInputValue(e.target.value)} />
+          value={counter}
+          onChange={(e) => setCounter(e.target.value)} /><br></br>
         <button type="submit">Save</button>
-
-        <h2>Current Value: {inputValue}</h2>
-        <h2>Previous Value: {previousInputValue.current}</h2>
       </form>
     </div>
-  )
+  );
 }
