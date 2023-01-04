44b91d7e7950322f8625b4ae38f9e13d41b04470
Em-Arce <emjenarce@hotmail.com>
2023-01-04 16:22:32 +0800

    Version correct counter behavior but not starting at db value

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,20 @@
 import React from "react";
 import { useState, useEffect, useRef } from "react"; 
 import { firestore } from "../firebase";
-import { doc, onSnapshot } from "@firebase/firestore";
+import { addDoc, collection, doc, 
+  query, orderBy, onSnapshot 
+} from "@firebase/firestore";
 
 export default function Home() {
   // const reference = collection(firestore, "counts");
 
+  // const [inputValue, setInputValue] = useState(0);
   const [currentValue, setCurrentValue] = useState(0);
   // const [counter, setCounter] = useState(0);
-  let counter = useRef(0);
+  
+  // let counter = useRef(currentValue);
+  
+
 
   // fetch data from firebase
   window.addEventListener( "load", () => {
@@ -22,37 +28,47 @@ export default function Home() {
       var data = doc.data().currentCount;
       setCurrentValue(data);
   });
+  // console.log(`current value ${currentValue}`);
+  const [counter, setCounter] = useState(currentValue);
+  // console.log(`counter value ${counter}`);
 
   // set the current counter to currentValue
-  useEffect(() => {
-    counter.current = currentValue;
-    // eslint-disable-next-line
-  }, [counter.current]);
+  // useEffect(() => {
+  //   counter.current = currentValue;
+  //   // eslint-disable-next-line
+  // }, [currentValue]);
+
+  // useEffect(()=> {
+  //   increment();
+  //   decrement();
+  //   reset();
+  // // eslint-disable-next-line
+  // }, [counter.current]);
+
 
   // console.log(counter)
+  
   const increment = () => {
-    // alert(`before increment current value ${counter.current}`)
-    counter.current += 1;
-    // alert(`after increment current value ${counter.current}`)
+    // console.log(`before increment current value ${counter}`);
+    setCounter(counter + 1);
+    // console.log(`after increment current value ${counter}`);
   };
 
   const decrement = () => {
-    counter.current -= 1;
+    // console.log(`before decrement current value ${counter}`);
+    setCounter(counter - 1);
+    // console.log(`after decrement current value ${counter}`);
   };
 
   const reset = () => {
-    counter.current = 0;
+    // console.log(`before reset current value ${counter}`);
+    setCounter(0)
+    // console.log(`after reset current value ${counter}`);
   };
 
-  useEffect(()=> {
-    increment();
-  //   decrement();
-  //   reset();
-  // eslint-disable-next-line
-  }, [counter.current]);
-
+  
   // let data = {
-  //   currentCount: currentValue,
+  //   currentCount: inputValue,
   // }
 
   // make this into update instead of save new entry
@@ -73,17 +89,17 @@ export default function Home() {
   return (
     <div>
       <h2>Previous Value from DB: {currentValue}</h2>
-      <h2>Counter Value: {counter.current}</h2>
+      <h2>Counter Value: {counter}</h2>
       <button onClick={increment}>+</button> <br></br>
       <button onClick={decrement}>-</button> <br></br>
       <button onClick={reset}>Reset</button>
       {/* <form onSubmit={handleSave}>
-        <label>Counter:</label>
+        <label>Input:</label>
         <input 
           type="number" 
-          value={currentValue}
+          value={inputValue}
           onChange={
-            (e) => (setCounter(e.target.value))
+            (e) => (setInputValue(e.target.value))
           } /><br></br>
         <button type="submit">Save</button>
       </form> */}
